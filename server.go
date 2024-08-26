9b6207e4a0c8ab30504c243e98508794ab54cbc3
Changyu Moon <121847433+window9u@users.noreply.github.com>
2024-08-26 17:38:18 +0900

    Add Logger

--- server.go
+++ server.go
@@ -10,14 +10,23 @@ import (
 func main() {
 	listenAddr := os.Getenv("LISTEN_ADDR")
 	if len(listenAddr) == 0 {
-		listenAddr = ":80"
+		listenAddr = ":8080"
 	}
+	logFile, err := os.OpenFile("./log.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer logFile.Close()
+	logger := log.New(logFile, "", log.Ldate|log.Ltime|log.Lmicroseconds)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!!")
-		fmt.Fprintln(os.Stdout, r)
+		fmt.Fprintf(w, "Hello %s!!", r.Host)
+		logger.Printf("host: %s, method: %s, url: %s", r.Host, r.Method, r.URL)
 	}))
 	fmt.Println("server starts to run")
+	logger.Printf("server run on %s", listenAddr)
 	log.Fatal(http.ListenAndServe(listenAddr, mux))
+	logger.Printf("server stop")
 }
